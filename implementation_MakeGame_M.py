8af886dc42da158b7e6284c18293cf6833d9be37
Ju-Young Han <jo00oh82@naver.com>
2024-10-29 17:30:38 +0900

    implementation

--- implementation_MakeGame_M.py
+++ implementation_MakeGame_M.py
@@ -1,10 +1,3 @@
-n = input()
-north = 0
-east = 1
-south = 2
-west = 3
-
-# row, col
 # 입력예시
 # 4 4
 # 1 1 0
@@ -12,3 +5,13 @@ west = 3
 # 1 0 0 1
 # 1 1 0 1
 # 1 1 1 1
+
+row, col = map(int, input().split())
+nx, ny, direction = map(int, input().split())
+for i in range(row):
+    data = list(map(int, input().split()))
+north = 0
+east = 1
+south = 2
+west = 3
+
