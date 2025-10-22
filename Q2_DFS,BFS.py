4a6ee679f014ffac6971c28df1a28cc4efc16b47
hjy <jo00oh82@naver.com>
2025-10-23 07:19:56 +0900

    algorithm

--- Q2_DFS,BFS.py
+++ Q2_DFS,BFS.py
@@ -29,22 +29,3 @@ def bfs(x, y):
     return graph[n-1][m-1]
 
 print(bfs(0, 0))
-
-
-
-n, m = map(int, input().split())
-
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y):
-    queue = deque()
-    queue.append(x, y)
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = 
